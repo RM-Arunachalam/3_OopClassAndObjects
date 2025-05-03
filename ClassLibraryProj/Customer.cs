414eb3454e823a775ec2d46e7110584c4b0e6519
Arunachalam_RM <Arunachalam_RM@outlook.com>
2025-05-03 10:00:39 +0530

    Add project files.

---
+++ ClassLibraryProj/Customer.cs
@@ -0,0 +1,15 @@
+﻿namespace ClassLibraryProj
+{
+    public class Customer
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+
+        public char grade;
+
+        public int getCustomerId()
+        {
+            return Id;
+        }
+    }
+}
---
+++ OopClassAndObjects/Program.cs
@@ -0,0 +1,17 @@
+﻿using ClassLibraryProj;
+
+namespace Programs
+{
+    class Sample
+    {
+        public static void Main(string[] args)
+        {
+            Customer customer = new Customer();//creating object 
+            customer.Id = 109;
+            Console.WriteLine("customerName=>"+customer.Name);// null
+            Console.WriteLine("customer grade=>"+customer.grade);// '\0'
+            Console.WriteLine("cus Id=>"+customer.Id);//109
+            
+        }
+    }
+}
